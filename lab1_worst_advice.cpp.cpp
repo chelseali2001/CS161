f08d63ba7e8a7c1c1c88dd41265e79cb09161559
Chelsea Li <chelseali2001@gmail.com>
2020-02-26 22:38:42 -0800

    Add files via upload

---
+++ lab1_worst_advice.cpp.cpp
@@ -0,0 +1,13 @@
+/***********************************************************
+** Program: lab1_worst_advice.cpp
+** Author: Chelsea Li
+** Date: 01/08/2020
+** Description: Print worst advice for a college freshman.
+***********************************************************/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Never go to class." << std::endl;
+	return 0;
+}
